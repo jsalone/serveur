3b9cbfdeb4e970451593b8ce41acbbf1107f735d
jonathan <jonathan.salone@imerir.com>
2017-06-23 14:16:28 +0200

    rejoindre partie

--- serveur.py
+++ serveur.py
@@ -58,7 +58,7 @@ def addPlayer():
 	taille = len(result)
 	if taille!= 0:
 		print("--------------------------------------invite---------------------------------------------------")
-		table['name'] = "invite%d"% 1
+		table['name'] = "invite%d"% invite+1
 	
 	idjoueur=db.select ("INSERT INTO joueur(JoueurNom, JoueurBudget) VALUES (%(name)s, 50) RETURNING idJoueur", {"name" : table["name"]})
 	result = db.select("SELECT idJoueur FROM joueur WHERE JoueurNom = %(name)s",{

3de3543744933be47edc2a26d2856b17a9de57e2
jonathan <jonathan.salone@imerir.com>
2017-06-23 10:19:42 +0200

    rejoindre partie

--- serveur.py
+++ serveur.py
@@ -59,7 +59,7 @@ def addPlayer():
 	
 	idjoueur=db.select ("INSERT INTO joueur(JoueurNom, JoueurBudget) VALUES (%(name)s, 50) RETURNING idJoueur as idJoueur", {"name" : table["name"]})
 	
-	db.select ("INSERT INTO magasin(MagasinPosX, MagasinPosY,idJoueur) VALUES (%(posX)d,%(posY)d,%(idJoueur)d) RETURNING idMagasin as magasin", {"posX" : random.randrange(10),"posY" : random.randrange(10),"idJoueur": idjoueur[0]['idJoueur']})
+	db.select ("INSERT INTO magasin(MagasinPosX, MagasinPosY,idJoueur) VALUES (%(posX)d,%(posY)d,%(idJoueur)d) RETURNING idMagasin as magasin", {"posX" : random.randrange(10),"posY" : random.randrange(10),"idJoueur": idjoueur[0]})
 	
     else:
         table['name'] = "Jacky"

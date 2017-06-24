f3deb62c424e40cff24b1f2ddf0fa9f4cffb5c1c
jonathan <jonathan.salone@imerir.com>
2017-06-24 15:58:12 +0200

    get ingredient

--- database_reset.sql
+++ database_reset.sql
@@ -82,7 +82,6 @@ CREATE TABLE joueur(
 CREATE TABLE recette(
         idRecette  int  Default nextval ('recette_seq')  NOT NULL ,
         RecetteNom Char (25) ,
-        idJoueur   Int ,
         PRIMARY KEY (idRecette )
 );
 
@@ -102,6 +101,14 @@ CREATE TABLE ingredient(
         PRIMARY KEY (idIngredient )
 );
 
+--------------------------------------------------------------
+-- Table: avoir
+--------------------------------------------------------------
+CREATE TABLE avoir(
+        idJoueur    Int NOT NULL ,
+        idRecette Int NOT NULL ,
+        PRIMARY KEY (idJoueur ,idRecette )
+);
 
 --------------------------------------------------------------
 -- Table: contenir
@@ -127,9 +134,24 @@ CREATE TABLE compatible(
 ALTER TABLE panneau ADD CONSTRAINT FK_panneau_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
 ALTER TABLE magasin ADD CONSTRAINT FK_magasin_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
 ALTER TABLE joueur ADD CONSTRAINT FK_joueur_IdPartie FOREIGN KEY (IdPartie) REFERENCES partie(IdPartie);
-ALTER TABLE recette ADD CONSTRAINT FK_recette_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
+
+ALTER TABLE avoir ADD CONSTRAINT FK_avoir_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
+ALTER TABLE avoir ADD CONSTRAINT FK_avoir_idRecette FOREIGN KEY (idRecette) REFERENCES recette(idRecette);
+
 ALTER TABLE contenir ADD CONSTRAINT FK_contenir_idRecette FOREIGN KEY (idRecette) REFERENCES recette(idRecette);
 ALTER TABLE contenir ADD CONSTRAINT FK_contenir_idIngredient FOREIGN KEY (idIngredient) REFERENCES ingredient(idIngredient);
 ALTER TABLE compatible ADD CONSTRAINT FK_compatible_idIngredient FOREIGN KEY (idIngredient) REFERENCES ingredient(idIngredient);
 ALTER TABLE compatible ADD CONSTRAINT FK_compatible_idIngredient_ingredient FOREIGN KEY (idIngredient_ingredient) REFERENCES ingredient(idIngredient);
+
+
+--------------------------------------------------------------
+-- Insert
+--------------------------------------------------------------
+
+INSERT INTO ingredient (IngredientNom,IngredientPrix,IngredientTemperature,IngredientAlcool) VALUES 
+   ('Eau', '0.5', '0', '0'),('orange', '1', '0', '0'),('graine de cafe', '2', '0', '0'),('légume', '4', '0', '0');
+INSERT INTO recette (RecetteNom) VALUES 
+   ('Eau'),('limonade'),('cafe'),('soupe');
+INSERT INTO avoir (idRecette,idIngredient) VALUES 
+   ('1','1'),('2','1'),('2','2'),('3','1'),('3','3'),('4','1'),('4','4');
 

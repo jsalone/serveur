b355d684612f523c8324c47b1048b7ad39ac2297
jonathan <jonathan.salone@imerir.com>
2017-06-24 14:26:09 +0200

    rejoindre partie

--- database_reset.sql
+++ database_reset.sql
@@ -1,135 +1,115 @@
---------------------------------------------------------------
---        Script MySQL.
---------------------------------------------------------------
-DROP TABLE IF EXISTS panneau CASCADE;
-DROP TABLE IF EXISTS magasin CASCADE;
-DROP TABLE IF EXISTS partie CASCADE;
-DROP TABLE IF EXISTS joueur CASCADE;
-DROP TABLE IF EXISTS recette CASCADE;
 DROP TABLE IF EXISTS ingredient CASCADE;
+DROP TABLE IF EXISTS recette CASCADE; 
+DROP TABLE IF EXISTS joueur CASCADE;
+DROP TABLE IF EXISTS panneauPub CASCADE; 
+DROP TABLE IF EXISTS taxe CASCADE;
+DROP TABLE IF EXISTS meteo CASCADE;
+DROP TABLE IF EXISTS carte CASCADE;
+
+/*Table déduite à partir des relations du modèle jmerise*/
 DROP TABLE IF EXISTS contenir CASCADE;
-DROP TABLE IF EXISTS compatible CASCADE;
+DROP TABLE IF EXISTS vendre CASCADE;
+DROP TABLE IF EXISTS faireAction CASCADE;
 
 
---------------------------------------------------------------
--- Table: panneau
---------------------------------------------------------------
-
-
-
-
---CREATE SEQUENCE panneau_seq;
-
-CREATE TABLE panneau(
-        idPanneau        int  Default nextval ('panneau_seq')  NOT NULL ,
-        PanneauPosX      Int ,
-        PanneauPosY      Int ,
-        PanneauInfluence Double precision ,
-        idJoueur         Int ,
-        PRIMARY KEY (idPanneau )
+/*Création des tables avec les données par défaut*/
+CREATE TABLE ingredient (
+	id_ingredient	SERIAL PRIMARY KEY,
+	nom_ingredient	Varchar(25),
+	prix_ingredient	Float
 );
 
-
---------------------------------------------------------------
--- Table: magasin
---------------------------------------------------------------
-
---CREATE SEQUENCE magasin_seq;
-
-CREATE TABLE magasin(
-        idMagasin   int  Default nextval ('magasin_seq')  NOT NULL ,
-        MagasinPosX Int ,
-        MagasinPosY Int ,
-        idJoueur    Int ,
-        PRIMARY KEY (idMagasin )
+CREATE TABLE recette (
+	id_recette		SERIAL PRIMARY KEY,
+	recette_nom		Varchar(25),
+	recette_prix_achat	Varchar(25)
 );
 
-
---------------------------------------------------------------
--- Table: partie
---------------------------------------------------------------
-
---CREATE SEQUENCE partie_seq;
-
-CREATE TABLE partie(
-        IdPartie  int  Default nextval ('partie_seq')  NOT NULL ,
-        PartieNom Char (25) ,
-        PRIMARY KEY (IdPartie )
+CREATE TABLE joueur (
+	id_joueur		SERIAL PRIMARY KEY,
+	pseudo_joueur		Varchar(25),
+	isConnected_joueur	Boolean,
+	positionX_joueur	Float,
+	positionY_joueur	Float,
+	latitude_joueur		Float,
+	longitude_joueur	Float
 );
 
-
---------------------------------------------------------------
--- Table: joueur
---------------------------------------------------------------
-
---CREATE SEQUENCE joueur_seq;
-
-CREATE TABLE joueur(
-        idJoueur     int  Default nextval ('joueur_seq')  NOT NULL ,
-        JoueurNom    Char (25) ,
-        JoueurBudget Double precision ,
-        IdPartie     Int ,
-        PRIMARY KEY (idJoueur )
+CREATE TABLE panneauPub (
+	id_panneauPub		SERIAL PRIMARY KEY,
+	prix_panneauPub		Float,
+	positionX_panneauPub	Float,
+	positionY_panneauPub	Float,
+	latitude_panneauPub	Float,
+	longitude_panneauPub	Float,
+/*Ici, il y a une foreign key a mettre maisplus tard*/
+	id_panneauPub_joueur	Int
 );
 
-
---------------------------------------------------------------
--- Table: recette
---------------------------------------------------------------
-
---CREATE SEQUENCE recette_seq;
-
-CREATE TABLE recette(
-        idRecette  int  Default nextval ('recette_seq')  NOT NULL ,
-        RecetteNom Char (25) ,
-        idJoueur   Int ,
-        PRIMARY KEY (idRecette )
+CREATE TABLE taxe (
+	id_taxe			SERIAL PRIMARY KEY,
+	tva_taxe		Float,
+	montant_fixe_taxe	Float,
+	jour_taxe		Float,
+/*Ici, il y a une foreign key a mettre maisplus tard*/
+	id_taxe_joueur		Int
 );
 
-
---------------------------------------------------------------
--- Table: ingredient
---------------------------------------------------------------
-
---CREATE SEQUENCE ingredient_seq;
-
-CREATE TABLE ingredient(
-        idIngredient          int  Default nextval ('ingredient_seq')  NOT NULL ,
-        IngredientNom         Char (25) ,
-        IngredientPrix        Double precision ,
-        IngredientTemperature Bool ,
-        IngredientAlcool      Bool ,
-        PRIMARY KEY (idIngredient )
+CREATE TABLE meteo (
+	id_meteo 		SERIAL PRIMARY KEY,
+	actuelle_meteo		Varchar(25),
+	previsionnelle_meteo	Varchar(25),
+	jour_meteo		Int
 );
 
-
---------------------------------------------------------------
--- Table: contenir
---------------------------------------------------------------
-
-CREATE TABLE contenir(
-        idRecette    Int NOT NULL ,
-        idIngredient Int NOT NULL ,
-        PRIMARY KEY (idRecette ,idIngredient )
+CREATE TABLE carte (
+	id_carte	SERIAL PRIMARY KEY,
+	longueur_carte	Float,
+	largeur_carte	Float
 );
 
-
---------------------------------------------------------------
--- Table: compatible
---------------------------------------------------------------
-
-CREATE TABLE compatible(
-        idIngredient            Int NOT NULL ,
-        idIngredient_ingredient Int NOT NULL ,
-        PRIMARY KEY (idIngredient ,idIngredient_ingredient )
+/*Contient deux clés étrangères des tables : recette et ingredient*/
+CREATE TABLE contenir (
+	id_contenir_recette	Int,
+	id_contenir_ingredient	Int
 );
 
-ALTER TABLE panneau ADD CONSTRAINT FK_panneau_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
-ALTER TABLE magasin ADD CONSTRAINT FK_magasin_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
-ALTER TABLE joueur ADD CONSTRAINT FK_joueur_IdPartie FOREIGN KEY (IdPartie) REFERENCES partie(IdPartie);
-ALTER TABLE recette ADD CONSTRAINT FK_recette_idJoueur FOREIGN KEY (idJoueur) REFERENCES joueur(idJoueur);
-ALTER TABLE contenir ADD CONSTRAINT FK_contenir_idRecette FOREIGN KEY (idRecette) REFERENCES recette(idRecette);
-ALTER TABLE contenir ADD CONSTRAINT FK_contenir_idIngredient FOREIGN KEY (idIngredient) REFERENCES ingredient(idIngredient);
-ALTER TABLE compatible ADD CONSTRAINT FK_compatible_idIngredient FOREIGN KEY (idIngredient) REFERENCES ingredient(idIngredient);
-ALTER TABLE compatible ADD CONSTRAINT FK_compatible_idIngredient_ingredient FOREIGN KEY (idIngredient_ingredient) REFERENCES ingredient(idIngredient);
+/*Contient deux clés étrangères des tables: recette et joueur*/
+CREATE TABLE vendre (
+	prix_vendre		Float,
+	id_vendre_recette	Int,
+	id_vendre_joueur	Int
 
+);
+
+/*contient deux clés étrangères des tables: recette et joueur*/
+CREATE TABLE faireAction (
+	id_action_recette	Int,
+	id_action_joueur	Int
+);
+
+/*Cette partie est a faire verifier et valider*/
+/*Introduction des relations et des clés dans chaque table*/
+/*Pour la table faireAction*/
+ALTER TABLE faireAction ADD CONSTRAINT FK_id_action_recette FOREIGN KEY (id_action_recette) REFERENCES recette(id_recette);
+ALTER TABLE faireAction ADD CONSTRAINT FK_id_action_joueur FOREIGN KEY (id_action_joueur) REFERENCES joueur(id_joueur);
+
+/*Pour la table vendre*/
+ALTER TABLE vendre ADD CONSTRAINT FK_id_vendre_recette FOREIGN KEY (id_vendre_recette) REFERENCES recette(id_recette);
+ALTER TABLE vendre ADD CONSTRAINT FK_id_vendre_joueur FOREIGN KEY (id_vendre_joueur) REFERENCES joueur(id_joueur);
+
+/*Pour la table contenir*/
+ALTER TABLE contenir ADD CONSTRAINT FK_id_contenir_recette FOREIGN KEY (id_contenir_recette) REFERENCES recette(id_recette);
+ALTER TABLE contenir ADD CONSTRAINT FK_id_contenir_ingredient FOREIGN KEY (id_contenir_ingredient) REFERENCES ingredient(id_ingredient);
+
+/*Pour la table panneauPub*/
+ALTER TABLE panneauPub ADD CONSTRAINT FK_id_panneauPub_joueur FOREIGN KEY (id_panneauPub_joueur) REFERENCES joueur(id_joueur);
+
+/*Pour la table taxe*/
+ALTER TABLE taxe ADD CONSTRAINT FK_id_taxe_joueur FOREIGN KEY (id_taxe_joueur) REFERENCES joueur(id_joueur);
+
+
+
+/*
+Ce message sert à voir les requete alter table
+https://www.postgresql.org/message-id/CALnrrJTfs=asHXkiGEX0GecG5HEX5rz5TEhpykerftniurVqfw@mail.gmail.com*/

f1a7b4f27d5f7d111c43b3e7b0f7bcc900ae7324
jonathan <jonathan.salone@imerir.com>
2017-06-26 18:46:12 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -309,7 +309,8 @@ def map():
 		mapItem[numjoueur]['location'][nbpan+1]['longitude']= mag[nbpan+1]['magasinposx']
 		mapItem[numjoueur]['influene'][nbpan+1]=mag[nbpan+1]['magasininfluence']
 	else:
-		mapItem['kind'][numjoueur]= 'stand'
+		
+		mapItem['kind']= 'stand'
 		mapItem[numjoueur]['owner']= playerName
 		mapItem[numjoueur]['location']={}
 		mapItem[numjoueur]['location']['latitude']=mag[0]['magasinposy']

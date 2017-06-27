49815b7e7782b5e3b53924e77306e1a9cecc8f13
jonathan <jonathan.salone@imerir.com>
2017-06-27 15:15:42 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -236,7 +236,7 @@ def actionsPlayer(playerName):
 	action['radius']*=10
 	print "---------------------------------",action['radius'][0]
 	print "---------------------------------",monjoueur[0]['joueurbudget']
-	if action['radius'][0]>monjoueur[0]['joueurbudget']:
+	if action['radius'][0]>int(monjoueur[0]['joueurbudget']):
 		fund={}
 		fund['sufficientFunds']= False
 		fund['totalCost']=action['radius']

7ce027a3a2e46049071e3b180196aed9d5787cf2
jonathan <jonathan.salone@imerir.com>
2017-06-28 14:32:40 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -231,7 +231,7 @@ def actionsPlayer(playerName):
 
 		if not idingr[0]['idingredient']:
 
-		else :
+		else:
 			contenir = db.select ("INSERT INTO contenir(idRecette,idIngredient) VALUES (%(idrec)s,%(iding)s) RETURNING idRecette", {"idrec" : idrecette[0]['idrecette'],"iding" : idingr[0]['idingredient'] })
     #ajout panneau
     if action['kind']=='ad':

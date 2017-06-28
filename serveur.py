22ea138719528d1480d138d601e376a1682fd400
jonathan <jonathan.salone@imerir.com>
2017-06-28 11:10:21 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -233,7 +233,7 @@ def actionsPlayer(playerName):
     if action['kind']=='ad':
 	print"--------------------------------------ad--------------------------------------------",action['radius'][0]
 	act=action['radius'][0]
-	act=(int)act*10
+	act=int(act)*10
 	print"--------------------------------------ad error1--------------------------------------------",act
 	if act>monjoueur[0]['joueurbudget']:
 		print"--------------------------------------ad error--------------------------------------------",act

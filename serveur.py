eb4181b867645aaecd474ee7b9da757d148fd91b
jonathan <jonathan.salone@imerir.com>
2017-06-28 18:00:33 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -417,10 +417,10 @@ def map():
 			print"--------------------------------------",drinksByPlayer
 		
 			mamap['map']['playerInfo'][newplayeurname]['drinksOffered'].append(drinksOffered)
+			print"--------------------------------------",mamap['map']['playerInfo'][newplayeurname]['drinksOffered']
 			mamap['map']['drinksByPlayer'][newplayeurname].append(drinksByPlayer)
 
 		mamap['map']['playerInfo'][newplayeurname]['sales'] = totalvend
-		
 		mamap['map']['playerInfo'][newplayeurname]['profit']=0
 
 

4c1387bbd36915b021481412f0f1b1cf829095b1
jonathan <jonathan.salone@imerir.com>
2017-06-29 12:01:26 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -267,6 +267,8 @@ def actionsPlayer(playerName):
 		fund['sufficientFunds']= False
 		fund['totalCost']=action['radius']
 		return jsonResponse(fund)
+	print"-----------------prix totla----------------",prixtotal
+	print"-----------------prix monjoueur----------------",monjoueur[0]['joueurbudget']
 	monjoueur[0]['joueurbudget']-=prixtotal
 	db.execute("UPDATE joueur SET JoueurBudget=(%(vd)s) WHERE idJoueur=%(name)s", {"recpri": monjoueur[0]['joueurbudget'],"vd": cost,"name" : monjoueur[0]['idjoueur']})
 	db.execute("UPDATE avoir SET vendre=(%(vd)s),recetteprix=(%(recpri)s) WHERE idRecette =%(idrect)s AND idJoueur=%(name)s", {"recpri": action['price'][keyboisson[0]],"vd": action['prepare'][keyboisson[0]],"idrect":idrecette[0]['idrecette'],"name" : monjoueur[0]['idjoueur']})

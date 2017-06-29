77ceb8eeb0d922427a97d4c64ee5698da291fe5d
jonathan <jonathan.salone@imerir.com>
2017-06-29 16:27:24 +0200

    get ingredient

--- serveur.py
+++ serveur.py
@@ -439,7 +439,11 @@ def map():
 			drinksByPlayer['isCold']=True
 			mamap['map']['playerInfo'][newplayeurname]['sales'] = totalvend
 			print"--------------------------------------",drinksByPlayer
-			mamap['map']['playerInfo'][newplayeurname]['drinksOffered'].append(drinksOffered['name'],drinksOffered['price'],drinksOffered['hasAlcohol'],drinksOffered['isCold'])
+			mamap['map']['playerInfo'][newplayeurname]['drinksOffered'].append(drinksOffered['name'])
+			mamap['map']['playerInfo'][newplayeurname]['drinksOffered'].append(drinksOffered['price'])
+			mamap['map']['playerInfo'][newplayeurname]['drinksOffered'].append(drinksOffered['hasAlcohol'])
+			mamap['map']['playerInfo'][newplayeurname]['drinksOffered'].append(drinksOffered['isCold'])
+
 			
 			print"------------------ajout--------------------",mamap['map']['playerInfo'][newplayeurname]['drinksOffered']
 			mamap['map']['drinksByPlayer'][newplayeurname].append(drinksByPlayer)

45766d7d6841a8d3cb9cdc99cc7d680a3942d5f0
jsalone <jonathan.salone@imerir.com>
2017-12-15 09:28:30 +0100

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -18,7 +18,7 @@ app.debug = True
 CORS(app)
 invite=0
 debutpartie=0
-
+error=json.dumps({'success':False}), 403, {'ContentType':'application/json'}
 @app.route('/reset')
 def route_dbinit():
   """Cette route sert à initialiser (ou nettoyer) la base de données."""
